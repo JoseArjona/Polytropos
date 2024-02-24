6bfe679f84f78e3ad2d364a7896992b1b6db3126
JoseArjona <j.angel.matheis@gmail.com>
2024-02-23 19:17:33 -0600

    first commit

---
+++ src/components/Switch.jsx
@@ -0,0 +1,40 @@
+import {useState, useEffect  } from 'react'
+
+
+export default function Switch(){
+  const [isCheck, setCheck] = useState(false)
+
+  const enabledDarkMode= () => {
+    localStorage.setItem("dark",true)
+    document.documentElement.setAttribute("data-theme", "dark");
+    setCheck(true);
+  }
+  
+  const disabledDarkMode = () => {
+    localStorage.setItem("dark", false)
+    document.documentElement.setAttribute("data-theme", "light");
+    setCheck(false);
+  }
+  
+  const handleSwitch = (e) => {
+    if (e.target.checked) enabledDarkMode()
+    if(!e.target.checked) disabledDarkMode()  
+  }
+  useEffect(() => {
+    if (localStorage.getItem("dark") === "true") {
+      enabledDarkMode()
+    }else{
+      disabledDarkMode()
+    }
+  }, [])
+
+  
+  return(
+  <label className="ui-switch">
+    { isCheck ? <input type="checkbox" onChange={handleSwitch} checked/> : <input type="checkbox" onChange={handleSwitch}/> }
+    <div className="slider">
+      <div className="circle"></div>
+    </div>
+  </label>
+  )
+}
